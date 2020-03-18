262d54ddeeae8febb4472dc4f70c3ad3a3f05549
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2020-03-18 18:17:32 +0100

    css fix on gif class

--- src/components/gif.js
+++ src/components/gif.js
@@ -5,8 +5,8 @@ const Gif = (props) => {
   const src = `https://media0.giphy.com/media/${props.gifId}/giphy.gif?cid=ecf05e47baeb0844d47727e435ac3be1bcd23f72159d80e9&rid=giphy.gif`
 
   return(
-    <div className={props.className} onClick={event => props.handleClick(props.gifId)}>
-      <img src={src} alt=""/>
+    <div  onClick={event => props.handleClick(props.gifId)}>
+      <img src={src} alt="" className={props.className}/>
     </div>
   )
 }
